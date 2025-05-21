cdf5c0a63cfaa0370518061bc89c294775f2e00a
igorvlima <igorveraslima3@gmail.com>
2025-05-21 19:54:06 -0300

    fix variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "Região AWS onde os recursos serão criados"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "db_username" {
   description = "Nome de usuário master do banco de dados"
   type        = string
